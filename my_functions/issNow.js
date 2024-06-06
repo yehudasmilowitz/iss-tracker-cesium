6df931989c6618617241563ae0a4496fa320850e
Yehuda Smilowitz <yehudasmilowitz@gmail.com>
2024-06-06 01:53:17 -0400

    don't use axios

--- my_functions/issNow.js
+++ my_functions/issNow.js
@@ -1,17 +1,36 @@
-const axios = require('axios');
+const http = require('http');
 
 exports.handler = async (event, context) => {
-  try {
-    const response = await axios.get('http://api.open-notify.org/iss-now.json');
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response.data)
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'api.open-notify.org',
+      path: '/iss-now.json',
+      method: 'GET'
     };
-  } catch (error) {
-    console.error('Error fetching ISS data:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'An error occurred while fetching ISS data' })
-    };
-  }
+
+    const req = http.request(options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          body: data
+        });
+      });
+    });
+
+    req.on('error', (error) => {
+      console.error('Error fetching ISS data:', error);
+      reject({
+        statusCode: 500,
+        body: JSON.stringify({ error: 'An error occurred while fetching ISS data' })
+      });
+    });
+
+    req.end();
+  });
 };
